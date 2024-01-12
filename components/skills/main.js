b17cce8c645d1804af3d411302ab6de1dd486e50
mr-35843 <mr-35843@proton.me>
2024-01-12 17:58:47 +0100

    main.js for skills component

---
+++ components/skills/main.js
@@ -0,0 +1,13 @@
+const skillsMainHeader = document.getElementById("skillsMainHeader");
+const skillsListOne = document.getElementById("skillsListOne");
+const skillsListTwo = document.getElementById("skillsListTwo");
+const skillsListThree = document.getElementById("skillsListThree");
+const skillsExtraContent = document.getElementById("skillsExtraContent");
+
+fetchTranslationJSON("skills").then(translation =>{
+    skillsMainHeader.innerHTML = translation.MAIN_HEADER;
+    skillsListOne.innerHTML = translation.LIST_ONE;
+    skillsListTwo.innerHTML = translation.LIST_TWO;
+    skillsListThree.innerHTML = translation.LIST_THREE;
+    skillsExtraContent = translation.EXTRA_CONTENT;
+})
